7983c76aa481b63b723de9a9fd6ad935438b8148
Tyler Jones <tyler@trybadge.com>
2026-08-24 12:29:44 -0700

    chore(deps): update vite to latest version

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,5 +2,5 @@ import {resolve} from "node:path";
 import {defineConfig} from "vite";
 
 export default defineConfig({
-  resolve: {alias: {"@badge-sdk/web": resolve(__dirname, "../src")}},
+  resolve: {alias: {"@badge-sdk/web": resolve(import.meta.dirname, "../src")}},
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import {defineConfig} from "vite";
 import dtsPlugin from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dtsPlugin({include: ["src"], rollupTypes: true})],
+  plugins: [dtsPlugin({include: ["src"], bundleTypes: true})],
   build: {
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(import.meta.dirname, "src/index.ts"),
       name: "badge",
       fileName: "index",
     },
